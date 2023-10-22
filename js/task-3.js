da5503fc26d5a82220c2cf4810d201e646037b32
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-10-22 21:24:09 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,21 @@
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+// Задача 3. Перевірка спаму
+
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    const forbiddenWords = lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+    if (forbiddenWords) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
