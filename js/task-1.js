4452e57958b59e8bebcabaf2bb8174c474939a49
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-10-22 21:24:02 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,24 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
+// Задача 1. Замовлення дроїдів
 
+// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів. Оголоси функцію makeTransaction(quantity, pricePerDroid, customerCredits), яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
+
+// Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// quantity — кількість замовлених дроїдів
+// pricePerDroid — ціна одного дроїда
+// customerCredits — сума коштів на рахунку клієнта
+
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) {
+        return `'Insufficient funds!'`;
+    } else {
+        return `'You ordered ${quantity} droids worth ${totalPrice} credits!'`
+    }
 }
 
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
-
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
